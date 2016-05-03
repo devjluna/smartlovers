598f05b6d71dc888c0963a675330f4f51a982b89
Jimmy Luna <devjluna@gmail.com>
2016-05-03 15:34:52 -0500

    subir home validado

--- src/js/vendor/module.js
+++ src/js/vendor/module.js
@@ -1,5 +1,6 @@
 var carousel = function() {
-	var owl = $('#owl-team');
+	var owl = $('#owl-team'),
+			owlP = $('#owl-promo')
 
 	owl.owlCarousel({
 		items: 3,
@@ -14,6 +15,28 @@ var carousel = function() {
 			},
 			480:{
 				items:1
+			},
+			768:{
+				items:3
+			},
+			1200:{
+				items:4
+			}
+		}
+	})
+	owlP.owlCarousel({
+		items: 3,
+		loop:true,
+		margin: 22,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+		responsive:{
+			0:{
+				items:1
+			},
+			480:{
+				items:2
 			},
 			768:{
 				items:3
